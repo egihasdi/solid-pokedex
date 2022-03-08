ac52d7465057a622ba33c97dd91430660c687d9d
Egi Hasdi <egi.hasdi@gmail.com>
2022-03-08 14:38:39 +0700

    setup graphql api

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,18 @@ import "windi.css";
 import { render } from "solid-js/web";
 import { Router } from "solid-app-router";
 import App from "./app";
+import { createClient, Provider } from 'solid-urql'
+
+const client = createClient({
+  url: "https://beta.pokeapi.co/graphql/v1beta"
+})
 
 render(
   () => (
     <Router>
-      <App />
+      <Provider value={client}>
+        <App />
+      </Provider>
     </Router>
   ),
   document.getElementById("root") as HTMLElement
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,30 +1,38 @@
-import { createSignal } from "solid-js";
+import { createSignal, createResource, For, Show } from "solid-js";
+import { createQuery} from 'solid-urql'
+
+const getPokemonQuery = `
+  query {
+    pokemon_v2_pokemon {
+      name
+    }
+  }
+`;
+
+const API = "https://pokeapi.co/api/v2";
+
+const Card = (props) => {
+  return (
+    <p>{props.pokemon.name}</p>
+  );
+}
 
 export default function Home() {
-  const [count, setCount] = createSignal(0);
+  const [items, itemsState, reexecuteQuery] = createQuery({
+    query: getPokemonQuery,
+  })
 
   return (
     <section class="bg-gray-100 text-gray-700 p-8">
-      <h1 class="text-2xl font-bold">Home</h1>
-      <p class="mt-4">This is the home page.</p>
-
-      <div class="flex items-center space-x-2">
-        <button
-          class="border rounded-lg px-2 border-gray-900"
-          onClick={() => setCount(count() - 1)}
-        >
-          -
-        </button>
-
-        <output class="p-10px">Count: {count}</output>
-
-        <button
-          class="border rounded-lg px-2 border-gray-900"
-          onClick={() => setCount(count() + 1)}
-        >
-          +
-        </button>
-      </div>
+      <Show when={!itemsState().fetching}>
+        <ul>
+          <For each={items().pokemon_v2_pokemon}>
+            {pokemon => 
+              <Card pokemon={pokemon}/>
+            }
+          </For>
+        </ul>
+      </Show>
     </section>
   );
 }
