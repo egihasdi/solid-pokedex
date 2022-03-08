d79b058314bcca3bee1838a1fbfb581f18861cc7
Egi Hasdi <egi.hasdi@gmail.com>
2022-03-08 15:20:19 +0700

    few styling, use types color reference, fetch img.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,24 +1,49 @@
 import { createSignal, createResource, For, Show } from "solid-js";
-import { createQuery} from 'solid-urql'
+import { createQuery } from 'solid-urql'
+import './home.css';
 
 const getPokemonQuery = `
   query {
-    pokemon_v2_pokemon {
+    species: pokemon_v2_pokemonspecies(
+      limit: 100
+      offset: 0
+      order_by: { id: asc }
+    ) {
+      id
       name
+      pokemons: pokemon_v2_pokemons {
+        id
+        types: pokemon_v2_pokemontypes {
+          type: pokemon_v2_type {
+            name
+          }
+        }
+      }
     }
   }
 `;
 
-const API = "https://pokeapi.co/api/v2";
-
 const Card = (props) => {
+  const type =  props.pokemon.pokemons[0].types[0].type.name
+  
   return (
-    <p>{props.pokemon.name}</p>
+    <div className={"bg-light--" + type}>
+      <img alt="" src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"+props.pokemon.id+".png"}/>
+      <p>{props.pokemon.name} {type}</p>
+      <p>-
+        <For each={props.pokemon.pokemons[0].types}>
+          {t => <span className={"bg--" + t.type.name}>
+            {t.type.name}
+          </span>}
+        </For>
+      </p>
+
+    </div>
   );
 }
 
 export default function Home() {
-  const [items, itemsState, reexecuteQuery] = createQuery({
+  const [items, itemsState] = createQuery({
     query: getPokemonQuery,
   })
 
@@ -26,9 +51,9 @@ export default function Home() {
     <section class="bg-gray-100 text-gray-700 p-8">
       <Show when={!itemsState().fetching}>
         <ul>
-          <For each={items().pokemon_v2_pokemon}>
-            {pokemon => 
-              <Card pokemon={pokemon}/>
+          <For each={items().species}>
+            {pokemon =>
+              <Card pokemon={pokemon} />
             }
           </For>
         </ul>
